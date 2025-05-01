489edc70596904719b405b311610f65c1d23fb5f
gwynejsn <gwynejustin.deterala.p@gmail.com>
2025-05-01 22:54:20 +0800

    initial to-do list with basic functions added

--- src/app/app-to-do/app-to-do.component.ts
+++ src/app/app-to-do/app-to-do.component.ts
@@ -9,11 +9,47 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app-to-do.component.css',
 })
 export class AppToDoComponent {
-  toDoList: string[] = ['okay'];
-  history: string[] = [];
+  schoolToDoList = [
+    { task: 'Do math homework', checked: false },
+    { task: 'Do dishes', checked: false },
+    { task: 'Do DSA', checked: false },
+  ];
 
-  isItemChecked = true;
-  putToTrash(index: number) {
-    console.log('trashing');
+  randomToDoList = [{ task: 'sleep...', checked: false }];
+
+  myemptyToDoList = [];
+
+  toDoCategory = [
+    { category: 'school', toDoList: this.schoolToDoList },
+    { category: 'random', toDoList: this.randomToDoList },
+    { category: 'myempty', toDoList: this.myemptyToDoList },
+  ];
+
+  categoryChosen: {
+    category: string;
+    toDoList: { task: string; checked: boolean }[];
+  } | null = this.toDoCategory[0];
+
+  changeCategory(event: Event) {
+    const newCategoryChosen =
+      this.toDoCategory.find(
+        (item) => item.category === (event.target as HTMLSelectElement).value
+      ) ?? null;
+    this.categoryChosen = newCategoryChosen ? { ...newCategoryChosen } : null;
+  }
+
+  newTaskInput = '';
+
+  addNewTask() {
+    if (this.newTaskInput.length > 0) {
+      this.toDoCategory
+        .find((item) => item.category === this.categoryChosen?.category)
+        ?.toDoList.push({
+          task: this.newTaskInput,
+          checked: false,
+        });
+
+      this.newTaskInput = '';
+    }
   }
 }
