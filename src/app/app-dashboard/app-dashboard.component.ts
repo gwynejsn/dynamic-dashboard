8e2976d2e4bc47e1a1ab8b0976b6e5e24716825a
gwynejsn <gwynejustin.deterala.p@gmail.com>
2025-05-09 18:41:19 +0800

    removing cards has been added

--- src/app/app-dashboard/app-dashboard.component.ts
+++ src/app/app-dashboard/app-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Type } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppToDoComponent } from '../app-to-do/app-to-do.component';
 
@@ -13,18 +13,23 @@ export class AppDashboardComponent {
   greeting = '';
   username = 'Gwyne';
 
+  addCardError = '';
+  removeCardLabel = 'remove';
+
   cardToBeAdded: string | null = null;
-  availableCards = [
+  availableCards: { cardName: string; card: any }[] = [
     {
       cardName: 'to-do',
       card: AppToDoComponent,
     },
   ];
-  cards: Type<any>[] = [AppToDoComponent];
+  cards: { cardName: string; card: any }[] = [this.availableCards[0]];
 
   // card actions
   showAddCardSelect = false;
   showDeleteCardSelect = false;
+  removingCard = false;
+  removeCardDisabled = false;
 
   constructor() {
     this.updateGreeting();
@@ -44,13 +49,53 @@ export class AppDashboardComponent {
   }
 
   addCard() {
+    this.addCardError = '';
     if (this.cardToBeAdded != null) {
       const cardFound = this.availableCards.find(
         (card) => card.cardName === this.cardToBeAdded
       );
 
-      if (cardFound) this.cards.push(cardFound.card);
-      this.showAddCardSelect = !this.showAddCardSelect;
+      let cardExist = false;
+      this.cards.forEach((card) => {
+        if (card.cardName == cardFound?.cardName) cardExist = true;
+      });
+
+      if (cardExist) {
+        console.log('already exist');
+        this.addCardError = 'card already exists.';
+      } else {
+        if (cardFound) this.cards.push(cardFound);
+        console.log(this.cards[0].cardName);
+        this.showAddCardSelect = !this.showAddCardSelect;
+      }
     }
+    if (this.cards.length > 0) this.removeCardDisabled = false;
+  }
+
+  setRemovingCard() {
+    if (!this.removingCard) {
+      this.removeCardLabel = 'cancel';
+      this.removingCard = true;
+    } else {
+      this.removeCardLabel = 'remove';
+      this.removingCard = false;
+    }
+  }
+
+  showAddCard() {
+    this.showAddCardSelect = true;
+    this.removingCard = false;
+    this.removeCardLabel = 'remove';
+  }
+
+  removeCard(cardName: string) {
+    this.removeCardLabel = 'cancel';
+    this.cards = this.cards.filter((card) => card.cardName !== cardName);
+    if (this.cards.length <= 0) this.removeCardDisabled = true;
+  }
+
+  cancelAddingCard() {
+    this.showAddCardSelect = false;
+    this.addCardError = '';
   }
 }
